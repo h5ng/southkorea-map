a71f50815ecb4dc69b329a5e5352e1df45f51442
h5ng <gilim.h5ng@gmail.com>
2017-10-27 15:25:33 +0900

    지도 줌 확대/축소 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
 import React, {Component} from 'react';
+import {geoMercator, geoPath} from 'd3-geo';
+import {zoom, zoomTransform} from 'd3-zoom';
+import {select} from 'd3-selection'
 import Map from './components/Map';
 import Navigation from './components/Navigation';
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            selected: '00',
-            lat: 35.86444105960784,
-            lng: 128.24005901199945,
-            scale: 5500,
-            width: 700,
-            height: 700
-        };
+        const selected = '00',
+            lat = 35.86444105960784,
+            lng = 128.24005901199945,
+            scale = 5500,
+            width = 700,
+            height = 700,
+            PROJECTION = geoMercator()
+                .translate([width / 2, height / 2])
+                .scale(scale)
+                .center([lng, lat]),
+            PATH = geoPath()
+                .projection(PROJECTION),
+            ZOOM = zoom()
+                .scaleExtent([1, 50])
+                .on('zoom', function () {
+                    let transform = zoomTransform(this),
+                        tx = Math.min(0, Math.max(transform.x, width - width * transform.k)),
+                        ty = Math.min(0, Math.max(transform.y, height - height * transform.k));
 
-        this.changeArea = this.changeArea.bind(this);
+                    transform.x = tx;
+                    transform.y = ty;
+
+                    select('.map-svg > g')
+                        .attr('transform', `translate(${tx}, ${ty})scale(${transform.k})`)
+                });
+
+        this.state = {selected, lat, lng, scale, width, height, PROJECTION, PATH, ZOOM};
     }
 
     render() {
@@ -23,12 +43,25 @@ class App extends Component {
                 <div>
                     <Navigation changeArea={this.changeArea}/>
                 </div>
-                <Map {...this.state}/>
+                <svg className="map-svg" width={this.state.width} height={this.state.height}>
+                    <Map {...this.state}/>
+                </svg>
+
             </div>
         );
     }
 
-    changeArea(selected) {
+    componentDidMount() {
+        console.log('component did mount!');
+        select('.map-svg')
+            .call(this.state.ZOOM);
+    }
+
+    componentDidUpdate() {
+        console.log('component did update!');
+    }
+
+    changeArea = selected => {
         if (!/^(?=[0-9]*$)(?:.{2}|.{5}|.{7})$/.test(selected)) {
             return alert('숫자 2, 5, 7 글자만');
         }
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,41 +1,32 @@
 import React from 'react';
-import {geoMercator, geoPath} from 'd3-geo';
+
 import {merge} from 'topojson-client'
 import {each, groupBy} from 'underscore';
 
 import '../assets/css/Map.css';
 import umd from '../assets/umd.json';
 
-const Map = ({width, height, scale, lat, lng, selected}) => {
-    const proj = geoMercator()
-            .translate([width / 2, height / 2])
-            .scale(scale)
-            .center([lng, lat]),
-        path = geoPath()
-            .projection(proj),
-        path_elements = [];
+const Map = ({selected, PATH}) => {
+    const path_elements = [],
+        grouping = groupBy(umd.objects.data.geometries, d => {
+            let code = d.properties.code,
+                sd_code = code.substring(0, 2),
+                sgg_code = code.substring(0, 5),
+                umd_code = code.substring(0, 7);
 
-    const grouping = groupBy(umd.objects.data.geometries, function (d) {
-        let code = d.properties.code,
-            sd_code = code.substring(0, 2),
-            sgg_code = code.substring(0, 5),
-            umd_code = code.substring(0, 7);
-
-        return sgg_code === selected ? umd_code : sd_code === selected.substring(0, 2) ? sgg_code : sd_code;
-    });
+            return sgg_code === selected ? umd_code : sd_code === selected.substring(0, 2) ? sgg_code : sd_code;
+        });
 
     each(grouping, (data, index) => {
         const merge_area = merge(umd, data);
         path_elements.push(
             <path className={selected === '00' ? 'selected' : index.includes(selected, 0) ? 'selected' : ''}
-                  key={index} d={path(merge_area)}></path>
+                  key={index} d={PATH(merge_area)}></path>
         );
     });
 
     return (
-        <svg width={width} height={height}>
-            {path_elements}
-        </svg>
+        <g>{path_elements}</g>
     );
 }
 
